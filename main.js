322908ff40d87cfb061f494dafac9a32147e50aa
marksmirnow <mark.smirnow2@gmail.com>
2021-12-16 22:23:39 +0300

    feat: add function draw & mouse events

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ drawField.height = window.innerHeight;
 
 const ctx = (drawField.getContext) ? drawField.getContext('2d') : 'Explorer sucks';
 console.log(ctx);
+
+let isDrawing = false;
+
+const draw = (e) => {
+
+	if (!isDrawing) return;
+	[drawX, drawY] = [e.offsetX, e.offsetY];
+	console.log(drawX, drawY);
+};
+
+drawField.addEventListener('mousemove', draw);
+drawField.addEventListener('mousedown', () => isDrawing = true);
+drawField.addEventListener('mouseup', () => isDrawing = false);
+drawField.addEventListener('mouseout', () => isDrawing = false);
