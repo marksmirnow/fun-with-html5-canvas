3d98fcd8725a92d714251b09a7ebc2867c375acb
marksmirnow <mark.smirnow2@gmail.com>
2021-12-17 01:37:01 +0300

    refactoring

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 const drawField = document.querySelector('.draw');
+const ctx = (drawField.getContext) ? drawField.getContext('2d') : 'Explorer sucks';
+
 drawField.width = window.innerWidth;
 drawField.height = window.innerHeight;
 
-const ctx = (drawField.getContext) ? drawField.getContext('2d') : 'Explorer sucks';
 ctx.lineWidth = 50;
 ctx.lineCap = 'round';
 ctx.strokeStyle = 'hsl(100, 100%, 50%)';
 
 let isDrawing = false;
-
 // * Start line coordinates
 let startDrawX = 0;
 let startDrawY = 0;
 
 let direction = true;
-
 let hue = 0;
+
 
 const draw = (e) => {
 
@@ -29,33 +29,24 @@ const draw = (e) => {
 	ctx.stroke();
 
 	// * Update begin coords
-	startDrawX = e.offsetX;
-	startDrawY = e.offsetY;
+	[startDrawX, startDrawY] = [e.offsetX, e.offsetY];
 
-	if (direction) {
-		ctx.lineWidth++;
-	} else {
-		ctx.lineWidth--;
-	}
+	direction ? ctx.lineWidth++ : ctx.lineWidth--;
 
-	if (ctx.lineWidth >= 100) {
-		direction = false;
-	}
-	if (ctx.lineWidth <= 1) {
-		direction = true;
+	if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+		direction = !direction;
 	}
 
 	hue++;
-	if (hue > 360) {
-		hue = 0;
-	}
+	hue = (hue > 360) ? 0 : hue;
 };
 
-drawField.addEventListener('mousemove', draw);
 drawField.addEventListener('mousedown', (e) => {
 	isDrawing = true;
 	// * Update begin coords when not drawing
 	[startDrawX, startDrawY] = [e.offsetX, e.offsetY];
 });
+
+drawField.addEventListener('mousemove', draw);
 drawField.addEventListener('mouseup', () => isDrawing = false);
 drawField.addEventListener('mouseout', () => isDrawing = false);
